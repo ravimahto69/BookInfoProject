de68db8c4db76c0c1b70d60ab183c32db30dbc95
ravimahto69 <krravi073@gmail.com>
2025-08-14 11:42:45 +0530

    intial commit

---
+++ src/Book.java
@@ -0,0 +1,14 @@
+public class Book {
+    public static void  main(String[] args){
+        String title ;
+        String author;
+        int publicationYear = 1925;
+        title = "TheGreatGatslby";
+        author ="F.scottFitzgerald";
+        System.out.println("Title of the Book is:- " +title);
+        System.out.println("The author of Book :-  "+author);
+        System.out.println("Publication of the Book is :-  "+ publicationYear);
+
+
+    }
+}
