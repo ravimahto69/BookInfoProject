0f9f4b0c7510e51578e55d97c267361a44ecfee3
ravimahto69 <krravi073@gmail.com>
2025-08-14 12:18:03 +0530

    Setup aCarInfoProject in car class

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Car {
+    public static void main(String[]args){
+        String make;
+        String model;
+        double mileage= 45_000.5D;
+        make ="Toyota";
+        model="camry";
+        System.out.println("Car Company Name :-  "+make);
+        System.out.println("Car model :-  "+model);
+        System.out.println("Car mileage :-  "+mileage);
+    }
 }
