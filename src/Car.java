3fb628e0b782eea30e5cbe5fd9e56ab60f63b084
Ravi mahto <krravi073@gmail.com>
2025-08-14 13:35:54 +0530

    Update Car.java

--- src/Car.java
+++ src/Car.java
@@ -1,12 +1,1 @@
-public class Car {
-    public static void main(String[]args){
-        String make;
-        String model;
-        double mileage= 45_000.5D;
-        make ="Toyota";
-        model="camry";
-        System.out.println("Car Company Name :-  "+make);
-        System.out.println("Car model :-  "+model);
-        System.out.println("Car mileage :-  "+mileage);
-    }
-}
+
